528c0b8bc98b57e3c52b360167ca6b8c32f8d888
ViniciusOBrito <peixonauta.vb@gmail.com>
2024-09-23 14:15:01 -0300

    init

---
+++ src/main/java/com/brito/bookdiary/BookDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.brito.bookdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDiaryApplication.class, args);
+    }
+
+}
